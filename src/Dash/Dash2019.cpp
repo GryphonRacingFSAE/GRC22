c6ac3bf05897d46c3660ceb6245b9073cf5f8825
Dallas Hart <36043275+Nomalah@users.noreply.github.com>
2023-01-27 09:56:17 -0500

    Change to unique_ptr and fix packaging on rpi

--- src/Dash/Dash2019.cpp
+++ src/Dash/Dash2019.cpp
@@ -1,20 +1,22 @@
 #include <QApplication>
 #include <QQmlApplicationEngine>
 #include <fmt/core.h>
+#include <memory>
 
 #include <AN400ECU.hpp>
 
-#ifdef QT_DEBUG
+#ifdef DEBUG
 #include <runtimeqml.hpp>
 #endif
 
 int main(int argc, char* argv[]) {
     QApplication app(argc, argv);
-    AN400ECU* ecu = new AN400ECU(); // Should be before QQml engine
+    std::string app_root = app.applicationDirPath().toStdString();
+    auto ecu = std::make_unique<AN400ECU>(app_root + "/AN400ECU.dbc"); // Should be before QQml engine
     QQmlApplicationEngine engine;
 
-    qmlRegisterSingletonInstance<AN400ECU>("CAN.AN400ECU", 1, 0, "AN400ECU", ecu);
-#ifdef QT_DEBUG
+    qmlRegisterSingletonInstance<AN400ECU>("CAN.AN400ECU", 1, 0, "AN400ECU", ecu.get());
+#ifdef DEBUG
     RuntimeQml* rt = new RuntimeQml(&engine);
     rt->parseQrc(ROOT_SOURCE_PATH "/qml.qrc");
     rt->setAutoReload(true); // Reload automatically on file update
--- src/Dash/main.cpp
+++ src/Dash/main.cpp
@@ -2,6 +2,7 @@
 #include <QQmlApplicationEngine>
 #include <QtGlobal>
 #include <fmt/core.h>
+#include <memory>
 
 #include <BMS.hpp>
 #include <EnergyMeter.hpp>
@@ -15,16 +16,16 @@ int main(int argc, char* argv[]) {
     QApplication app(argc, argv);
     std::string app_root = app.applicationDirPath().toStdString();
     // Should be before QQml engine
-    MotorController* motor_controller = new MotorController(app_root + "/20220510_Gen5_CAN_DB.dbc");
-    EnergyMeter* energy_meter = new EnergyMeter(app_root + "/Energy_Meter_CAN_Messages.dbc");
-    BMS* bms = new BMS(app_root + "/Orion_CANBUS.dbc");
+    auto motor_controller = std::make_unique<MotorController>(app_root + "/20220510_Gen5_CAN_DB.dbc");
+    auto energy_meter = std::make_unique<EnergyMeter>(app_root + "/Energy_Meter_CAN_Messages.dbc");
+    auto bms = std::make_unique<BMS>(app_root + "/Orion_CANBUS.dbc");
 
     QQmlApplicationEngine engine;
 
     qmlRegisterSingletonInstance<MotorController>(
-        "CAN.MotorController", 1, 0, "MotorController", motor_controller);
-    qmlRegisterSingletonInstance<EnergyMeter>("CAN.EnergyMeter", 1, 0, "EnergyMeter", energy_meter);
-    qmlRegisterSingletonInstance<BMS>("CAN.BMS", 1, 0, "BMS", bms);
+        "CAN.MotorController", 1, 0, "MotorController", motor_controller.get());
+    qmlRegisterSingletonInstance<EnergyMeter>("CAN.EnergyMeter", 1, 0, "EnergyMeter", energy_meter.get());
+    qmlRegisterSingletonInstance<BMS>("CAN.BMS", 1, 0, "BMS", bms.get());
 #ifdef DEBUG
     RuntimeQml* rt = new RuntimeQml(&engine);
     rt->parseQrc(ROOT_SOURCE_PATH "/qml.qrc");
