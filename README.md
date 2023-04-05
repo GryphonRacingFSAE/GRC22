a8b45cd05b3aa4f3d33362e072376dc61e46d465
Evan Rutten <etrutten@gmail.com>
2023-04-04 23:15:05 -0400

    TWAI loopback test

--- README.md
+++ README.md
@@ -1,15 +1,16 @@
 # SMB-Firmware
+
 Firmware for segment monitor boards. Utilizes an STM32F103x8 µC to monitor temperatures across a segment. Conforms to Orion BMS TEM [CAN specification](https://www.orionbms.com/products/thermistor-expansion-module).
 
 ## CAN Specification
 
-| CAN ID* | Description | Broadcast Period [ms] | Length |
-|-|-|-|-|
-| 18EEFF80 | J1939 Address Claim Broadcast | 200 | 8 |
-| 1839F380 | BMS Thermistor Broadcast | 100 | 8 |
-| 1838F380 | General Thermistor Broadcast | 100 | 8 |
-| 18400080 | Unknown | 100 | 8 |
-| 18408000 | Unknown | 100 | 8 |
+| CAN ID\* | Description                   | Broadcast Period [ms] | Length |
+| -------- | ----------------------------- | --------------------- | ------ |
+| 18EEFF80 | J1939 Address Claim Broadcast | 200                   | 8      |
+| 1839F380 | BMS Thermistor Broadcast      | 100                   | 8      |
+| 1838F380 | General Thermistor Broadcast  | 100                   | 8      |
+| 18400080 | Unknown                       | 100                   | 8      |
+| 18408000 | Unknown                       | 100                   | 8      |
 
 | 80 | Legacy - RESERVED | 100 | 4 |
 
@@ -17,5 +18,6 @@ Firmware for segment monitor boards. Utilizes an STM32F103x8 µC to monitor temp
 - View the [CAN specification](https://www.orionbms.com/products/thermistor-expansion-module) for the Orion BMS TEMs for more info
 
 Note
+
 - F3 in the table is the BMS j1939 Address
 - Last two digits of CAN ID is the CAN ID of the thermistor expansion model
---
+++ src/main.c
@@ -0,0 +1,71 @@
+#include <stdio.h>
+#include "driver/twai.h"
+
+void app_main()
+{
+    twai_general_config_t g_config = TWAI_GENERAL_CONFIG_DEFAULT(GPIO_NUM_21, GPIO_NUM_22, TWAI_MODE_NO_ACK);
+    twai_timing_config_t t_config = TWAI_TIMING_CONFIG_500KBITS();
+    twai_filter_config_t f_config = TWAI_FILTER_CONFIG_ACCEPT_ALL();
+
+    if (twai_driver_install(&g_config, &t_config, &f_config) == ESP_OK)
+    {
+        printf("Driver installed\n");
+    }
+    else
+    {
+        printf("Failed to install driver\n");
+        return;
+    }
+
+    if (twai_start() == ESP_OK)
+    {
+        printf("Driver started\n");
+    }
+    else
+    {
+        printf("Failed to start driver\n");
+        return;
+    }
+
+    twai_message_t tx_msg;
+    twai_message_t rx_msg;
+
+    while (true)
+    {
+        tx_msg.identifier = 0x123;
+        tx_msg.data_length_code = 4;
+        tx_msg.data[0] = 0xAA;
+        tx_msg.data[1] = 0xBB;
+        tx_msg.data[2] = 0xCC;
+        tx_msg.data[3] = 0xDD;
+
+        if (twai_transmit(&tx_msg, pdMS_TO_TICKS(1000)) == ESP_OK)
+        {
+            printf("Message queued for transmission\n");
+        }
+        else
+        {
+            printf("Failed to queue message for transmission\n");
+        }
+
+        if (twai_receive(&rx_msg, pdMS_TO_TICKS(10000)) == ESP_OK)
+        {
+            printf("Message received\n");
+
+            printf("ID: 0x%lx  Data: ", rx_msg.identifier);
+            if (!(rx_msg.rtr))
+            {
+                for (int i = 0; i < rx_msg.data_length_code; i++)
+                {
+                    printf(" %02X", rx_msg.data[i]);
+                }
+            }
+            printf("\n");
+        }
+        else
+        {
+            printf("Failed to receive message\n");
+            return;
+        }
+    }
+}
