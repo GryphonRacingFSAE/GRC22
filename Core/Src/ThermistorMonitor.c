1febeab3b89ed0e551cc90add45bb55178901330
Ayden Bourdeau <aydenbourdeau25@gmail.com>
2023-05-01 13:03:49 -0400

    Updated Magic Numbers
    - Updated magic numbers into thermistor monitor task

--- Core/Src/ThermistorMonitor.c
+++ Core/Src/ThermistorMonitor.c
@@ -8,15 +8,21 @@ extern ADC_HandleTypeDef hadc1;
 ThermistorData_Struct ThermistorData = {};
 
 void startThermistorMonitorTask() {
+
 	uint32_t tick = osKernelGetTickCount();
+	uint32_t bit_mask = 0b111;
+	uint32_t ADC_Res = 4095;
 	uint8_t counter = 0;
+
+	float abs_zero = 273.15;
 	float R0 = 10000;
 	float R1= 10000;
 
-
 	while (1) {
 
-		counter = (counter+1) & 7;
+		//Counter Variable for MUX Select Lines
+		counter = (counter + 1) & bit_mask;
+
 		HAL_GPIO_WritePin(GPIOA, 0b111000, GPIO_PIN_RESET);
 	    HAL_GPIO_WritePin(GPIOA, counter<<3, GPIO_PIN_SET);
 
@@ -28,12 +34,12 @@ void startThermistorMonitorTask() {
 		uint32_t Vt = HAL_ADC_GetValue(&hadc1);
 
 	    //Obtain Thermistor Resistance from Measured Voltage
-	    float R2 = (R1 * (4095.0/ (4095.0-(float)Vt)-1.0));
+	    float R2 = (R1 * ((ADC_Res)/ (ADC_Res - (float)Vt) - 1.0));
 
 		//Convert Thermistor Resistance to Temperature
-	    int32_t temp = ((1.0/((1.0/298.15)+((1.0/3435)*log(R2/R0))))-273.15);
+	    int32_t temp = ((1.0/((1.0/(abs_zero + 25))+((1.0/3435)*log(R2/R0))))-abs_zero);
 
-		GRCprintf("VT = %d, Channel %d Resistance = %d Temperature = %ld\n",Vt,counter,((uint32_t)R2),temp);
+	    GRCprintf("VT = %d, Channel %d Resistance = %d Temperature = %ld\n",Vt,counter,((uint32_t)R2),temp);
 
 		osDelayUntil(tick += THERMISTOR_PERIOD);
 	}
