ae6c50f64d6aac9186d609ee5ffd7c101e5b52dd
Yusuf <yusuf.fesan@gmail.com>
2023-11-17 17:36:01 -0500

    refactor: replace all indents with tabs

--- Core/Src/ThermistorMonitor.c
+++ Core/Src/ThermistorMonitor.c
@@ -80,75 +80,60 @@ void startThermistorMonitorTask() {
 
 		osDelay(100);
 
-        // Grab the temperature for all modules at once
-        for (uint8_t cur_mux = 0; cur_mux < MUX_COUNT; cur_mux++) {
-        	// changes the MUX that the ADC polls
-        	adcChangeMUX(cur_mux);
-            HAL_ADC_Start(&hadc1);
-            HAL_ADC_PollForConversion(&hadc1, HAL_MAX_DELAY);
-            uint32_t divider_output = HAL_ADC_GetValue(&hadc1);
-            HAL_ADC_Stop(&hadc1);
+		// Grab the temperature for all modules at once
+		for (uint8_t cur_mux = 0; cur_mux < MUX_COUNT; cur_mux++) {
+			// changes the MUX that the ADC polls
+			adcChangeMUX(cur_mux);
+			HAL_ADC_Start(&hadc1);
+			HAL_ADC_PollForConversion(&hadc1, HAL_MAX_DELAY);
+			uint32_t divider_output = HAL_ADC_GetValue(&hadc1);
+			HAL_ADC_Stop(&hadc1);
 
 
-            DEBUG_PRINT("----------------------------\n");
-            DEBUG_PRINT("Divider ADC out: %d\n", divider_output);
-//
-//
-            // converts ADC units to volts
-            float divider_voltage = ((float) divider_output/(float) ADC_RESOLUTION * REFERENCE_VOLTAGE);
-//
-//
-            DEBUG_PRINT("Divider Voltage: %d.%d\n", (int)divider_voltage, (int) (divider_voltage * 1000) % 1000);
-//
-            // calculates resistance of the thermistor using rearrangement of voltage divider formula: https://ohmslawcalculator.com/voltage-divider-calculator
-            float thermistor_resistance = (divider_voltage * DIVIDER_RESISTANCE) / (REFERENCE_VOLTAGE - divider_voltage);
-//
-            DEBUG_PRINT("Thermistor Resistance: %d\n", (int) (thermistor_resistance));
+			DEBUG_PRINT("----------------------------\n");
+			DEBUG_PRINT("Divider ADC out: %d\n", divider_output);
 
+			// converts ADC units to volts
+			float divider_voltage = ((float) divider_output/(float) ADC_RESOLUTION * REFERENCE_VOLTAGE);
 
-		   // TODO: pick a better sentinel value
-            if (thermistor_resistance == 0) {
-            	ThermistorData.thermistors[cur_mux][select_line] = -1000;
-            	continue;
-            }
+			DEBUG_PRINT("Divider Voltage: %d.%d\n", (int)divider_voltage, (int) (divider_voltage * 1000) % 1000);
+
+			// calculates resistance of the thermistor using rearrangement of voltage divider formula: https://ohmslawcalculator.com/voltage-divider-calculator
+			float thermistor_resistance = (divider_voltage * DIVIDER_RESISTANCE) / (REFERENCE_VOLTAGE - divider_voltage);
+
+			DEBUG_PRINT("Thermistor Resistance: %d\n", (int) (thermistor_resistance));
+
+			// TODO: pick a better sentinel value
+			if (thermistor_resistance == 0) {
+				ThermistorData.thermistors[cur_mux][select_line] = -1000;
+				continue;
+			}
 
 			// Convert Thermistor Resistance to Temperature
-			// This is a rearrangement of the equation found here: https://www.lasercalculator.com/ntc-thermistor-calculator/
-			float therm_temp = (float) (CALIBRATION_TEMPERATURE * THERMISTOR_BETA) / ((CALIBRATION_TEMPERATURE*log(thermistor_resistance/CALIBRATION_RESISTANCE)) + THERMISTOR_BETA);
-//			DEBUG_PRINT("Thermistor Temperature (NTC appoximation)(K): %d.%d\n", (int) (therm_temp), (int) ((int) (therm_temp * 100) % 100));
-//
-//			// This is an equation which is can be more accurate than generalizing the shape of the temperature-resistange curve, found here: https://www.ametherm.com/thermistor/ntc-thermistors-steinhart-and-hart-equation
+			// This is an equation which is can be more accurate than generalizing the shape of the temperature-resistange curve, found here: https://www.ametherm.com/thermistor/ntc-thermistors-steinhart-and-hart-equation
 			float stein_temp = 1.0 / (TEMP_COEFF_A + (TEMP_COEFF_B * log(thermistor_resistance/(float) CALIBRATION_RESISTANCE)) + TEMP_COEFF_C * powf(log(thermistor_resistance/(float) CALIBRATION_RESISTANCE), 2.0) + TEMP_COEFF_D * powf(log(thermistor_resistance/(float) CALIBRATION_RESISTANCE), 3.0));
 			DEBUG_PRINT("Thermistor Temperature (Steinhart & Hart Coeff) (K): %d.%d\n", (int) (stein_temp), (int) ((int) (stein_temp * 100) % 100));
 
-//			float stein_resistance = CALIBRATION_RESISTANCE * exp(RES_COEFF_A + RES_COEFF_B/stein_temp + RES_COEFF_C/powf(stein_temp, 2.0) + RES_COEFF_D/powf(stein_temp, 3.0));
-//			DEBUG_PRINT("Thermistor Accurate Resistance (Stein): %d.%d", (int)((stein_resistance*100)/100), (int)((int) (stein_resistance*100)%100));
+			float stein_temp_celsius = stein_temp - 273.15f;
 
-			// kelvin to celsius conversion
-            float temp_celsius = therm_temp - 273.15f;
-            float stein_temp_celsius = stein_temp - 273.15f;
+			DEBUG_PRINT("Thermistor Temperature (SteinHart & Hart) (C): %d.%d\n", (int) (stein_temp_celsius), abs((int) ((int) (stein_temp_celsius * 100) % 100)));
+			DEBUG_PRINT("Thermistor channel: %d\n", select_line);
+			DEBUG_PRINT("Current MUX: %d\n", cur_mux);
 
-//            DEBUG_PRINT("Thermistor Temperature (C): %d.%d\n", (int) (temp_celsius), abs((int) ((int) (temp_celsius * 100) % 100)));
-            DEBUG_PRINT("Thermistor Temperature (SteinHart & Hart) (C): %d.%d\n", (int) (stein_temp_celsius), abs((int) ((int) (stein_temp_celsius * 100) % 100)));
-            DEBUG_PRINT("Thermistor channel: %d\n", select_line);
-            DEBUG_PRINT("Current MUX: %d\n", cur_mux);
-
-            //GRCprintf("Channel: %d, Count: %d, Temp: %d, Resistance: %d, Percent of voltage: %d, Measured Voltage: %d\n", (int32_t)cur_mux, (int32_t)select_line, thermistor_temperature, (int32_t)thermistor_resistance, (int32_t)(percent_of_divider_input_voltage * 100), divider_voltage);
-
-            ThermistorData.thermistors[cur_mux][select_line] = therm_temp;
-        }
+			ThermistorData.thermistors[cur_mux][select_line] = therm_temp;
+		}
 
 		//Counter Variable for MUX Select Lines
-        if (DEV_BOARD == 0) {
-			if (++select_line == THERMISTORS_PER_MUX) {
-				select_line = 0;
-			}
+        if (!DEV_BOARD) {
+        	if (++select_line == THERMISTORS_PER_MUX) {
+        		select_line = 0;
+        	}
         } else {
-			if (select_line == 0) {
-				select_line = 3;
-			} else {
-				select_line = 0;
-			}
+        	if (select_line == 0) {
+        		select_line = 3;
+        	} else {
+        		select_line = 0;
+        	}
         }
 
         CANTXMsg bms_broadcast = {
