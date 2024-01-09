4005ac6ac0ff8c882f7a1d08483d165f34a50f6a
Dallas Hart <36043275+Nomalah@users.noreply.github.com>
2024-01-09 13:18:30 -0500

    style: clang & cmake format

--- software/src/Dash/fake.cpp
+++ software/src/Dash/fake.cpp
@@ -15,16 +15,14 @@ int main(int argc, char* argv[]) {
     QApplication app(argc, argv);
     std::string app_root = app.applicationDirPath().toStdString();
     // Should be before QQml engine
-    fake::MotorController* motor_controller =
-        new fake::MotorController(app_root + "/DBCs/20220510_Gen5_CAN_DB.dbc");
+    fake::MotorController* motor_controller = new fake::MotorController(app_root + "/DBCs/20220510_Gen5_CAN_DB.dbc");
     fake::EnergyMeter* energy_meter = new fake::EnergyMeter(app_root + "/DBCs/Energy_Meter_CAN_Messages.dbc");
     fake::BMS* bms = new fake::BMS(app_root + "/DBCs/Orion_CANBUS.dbc");
     fake::VCU* vcu = new fake::VCU(app_root + "/TorqueMaps");
 
     QQmlApplicationEngine engine;
 
-    qmlRegisterSingletonInstance<fake::MotorController>(
-        "CAN.MotorController", 1, 0, "MotorController", motor_controller);
+    qmlRegisterSingletonInstance<fake::MotorController>("CAN.MotorController", 1, 0, "MotorController", motor_controller);
     qmlRegisterSingletonInstance<fake::EnergyMeter>("CAN.EnergyMeter", 1, 0, "EnergyMeter", energy_meter);
     qmlRegisterSingletonInstance<fake::BMS>("CAN.BMS", 1, 0, "BMS", bms);
     qmlRegisterSingletonInstance<fake::VCU>("CAN.VCU", 1, 0, "VCU", vcu);
