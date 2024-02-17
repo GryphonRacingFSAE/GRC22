a616d615e79f76b8c8aefc36e6d3b5e9d65186ca
Dallas Hart <36043275+Nomalah@users.noreply.github.com>
2024-02-17 00:30:34 -0500

    fix: compile issues

--- Core/Src/watchdog.c
+++ Core/Src/watchdog.c
@@ -12,7 +12,9 @@
 #include "main.h"
 
 Watchdog_Data_Struct Watchdog_Data = {
-	.flags = BSPC_INVALID | RTD_INVALID // This must contain all errors that can be reset
+	.flags = BSPC_INVALID | RTD_INVALID, // This must contain all errors that can be reset
+	.tick_stamp = {},
+	.tick_limit = {}
 };
 
 void startWatchdogTask() {
@@ -23,12 +25,12 @@ void startWatchdogTask() {
 		// Watch RTD, BSPC, T.4.2.5, T.4.3.3 and disable motor controller after the "delay time", and send notification message over CAN
 
 		uint32_t flags = Watchdog_Data.flags;
-		uint32_t flags2 = 0;
+//		uint32_t flags2 = 0;
 		while (flags) {
 			// Safe as flags is never 0 here
 			uint32_t flag_index = 31 - __builtin_clz(flags);
 
-			if ((tick - tick_stamp[flag_index]) > tick_limit[flag_index]) {
+			if ((tick - Watchdog_Data.tick_stamp[flag_index]) > Watchdog_Data.tick_limit[flag_index]) {
 
 			}
 
