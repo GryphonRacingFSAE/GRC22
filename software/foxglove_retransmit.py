e40407f4d01a1a3ca3e6e90349865c2bfafdec7f
Dallas Hart <36043275+Nomalah@users.noreply.github.com>
2024-03-02 22:16:54 -0500

    chore: progress update for restransmitting server

---
+++ software/foxglove_retransmit.py
@@ -0,0 +1,42 @@
+from hytech_np_proto_py import hytech_pb2
+import google.protobuf.message_factory
+from cantools.database import *
+
+import asyncio
+import websocket
+
+
+def on_message(wsapp, message):
+    print(message)
+
+def get_msg_names_and_classes():
+    message_names = []
+    message_classes = {}
+    # Iterate through all attributes in the generated module
+    for attr_name in dir(hytech_pb2):
+        # Check if the attribute is a class and if it's a message type
+        attr = getattr(hytech_pb2, attr_name)
+        if isinstance(attr, type) and hasattr(attr, "DESCRIPTOR"):
+            message_names.append(attr.DESCRIPTOR.name)
+            message_classes[
+                attr.DESCRIPTOR.name
+            ] = google.protobuf.message_factory.GetMessageClass(
+                hytech_pb2.DESCRIPTOR.message_types_by_name.get(attr.DESCRIPTOR.name)
+            )
+    return message_names, message_classes
+
+
+def pack_protobuf_msg(cantools_dict: dict, msg_name: str, message_classes):
+    if msg_name in message_classes:
+        pb_msg = message_classes[msg_name]()
+    for key in cantools_dict.keys():
+        if(type(cantools_dict[key]) is namedsignalvalue.NamedSignalValue):
+            setattr(pb_msg, key, cantools_dict[key].value)
+        else:
+            setattr(pb_msg, key, cantools_dict[key])
+    return pb_msg
+
+    
+if __name__ == "__main__":
+    with websocket.WebSocketApp("ws://192.168.4.1:8765", on_message=on_message) as wsapp:
+        wsapp.run_forever()
