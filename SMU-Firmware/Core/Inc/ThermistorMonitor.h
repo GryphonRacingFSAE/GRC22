c637e390e4250c94bf8b2096fbf283540834813d
Evan Rutten <contact@evanrutten.com>
2024-07-15 12:48:21 -0400

    Merged SMU-Firmware into subfolder

---
+++ SMU-Firmware/Core/Inc/ThermistorMonitor.h
@@ -0,0 +1,46 @@
+#ifndef INC_THERMISTORMONITOR_H_
+#define INC_THERMISTORMONITOR_H_
+
+#include "cmsis_os2.h"
+
+// 2ms between measurements
+#define THERMISTOR_PERIOD 50
+
+#define DEV_BOARD 0
+
+#if DEV_BOARD == 0
+#define MUX_COUNT 4
+#define MULTIPLEXER_SIZE 16
+#define THERMISTORS_PER_MUX 14
+
+// This has to be done because Chris and Zohair didn't listen/forgot logic.
+static const uint16_t UNPOPULATED_THERMISTORS[MUX_COUNT] = {
+	0x8100,
+	0x00C0,
+	0x0003,
+	0x0003,
+};
+
+#else
+#define MUX_COUNT 1
+#define THERMISTORS_PER_MUX 2
+#endif
+
+#define THERMISTOR_COUNT (THERMISTORS_PER_MUX * MUX_COUNT)
+
+// Thermistor data is stored in a 2D array formatted as such:
+// [Multiplexer ID (from ADC channel; e.g.: 0, 1, 2, etc)] ->
+// [Thermistor of Multiplexer (based on select line of MUX; e.g: 0-7 for 8 channel MUX and 0-15 for 6 channel MUX)]
+typedef struct {
+    int8_t thermistors[THERMISTOR_COUNT];
+} ThermistorData_Struct;
+
+
+
+extern ThermistorData_Struct ThermistorData;
+
+void ADC_Select_MUX(int channel);
+
+void startThermistorMonitorTask();
+
+#endif /* INC_THERMISTORMONITOR_H_ */
